7be79657574b1314040b0b8c5bc63164c7fa6f67
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-11-04 09:00:55 +0800

    removed vue-rollbar

--- src/boot/error-handler.js
+++ src/boot/error-handler.js
@@ -1,11 +1,21 @@
+import Rollbar from 'rollbar';
 
 export default async ({ Vue }) => {
+  Vue.prototype.$rollbar = new Rollbar({
+    accessToken: '87b61aeb039b44a8b3269a70ef0f4d9b',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    enabled: true,
+    environment: 'production'
+  });
+
   Vue.config.errorHandler = (err, vm, info) => {
+    vm.$rollbar.error(err);
+    throw err; // rethrow
     // err: error trace
     // vm: component in which error occured
     // info: Vue specific error information such as lifecycle hooks, events etc.
 
     // TODO: Perform any custom logic or log to server
-    
   };
 }
